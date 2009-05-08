0c77db3414c1392a880cb4e639422c94889fa45c
Pavel Kunc <pavel.kunc@gmail.com>
2009-05-09 00:25:56 +0100

    Extra docs.

--- spec/spec_model.rb
+++ spec/spec_model.rb
@@ -1,6 +1,8 @@
+# Dummy model for testing
 class SpecModel < Sequel::Model
 end
 
+# Migrations to create the table
 class CreateSpecModel < Sequel::Migration
   def up
     create_table! :spec_models do
@@ -14,6 +16,7 @@ class CreateSpecModel < Sequel::Migration
   end
 end
 
+# Shared describe to run migration
 describe "it has a SpecModel", :shared => true do
   before(:each) do
     CreateSpecModel.apply(SpecModel.db, :up)
